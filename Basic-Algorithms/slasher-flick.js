e5c22b9a5911d7b8999b5b314a19685afb3ba287
Zachary Bennett <zbennett10@Hotmail.com>
2016-12-06 15:45:32 -0500

    Create slasher-flick.js

---
+++ Basic-Algorithms/slasher-flick.js
@@ -0,0 +1,9 @@
+function slasher(arr, n) {
+  arr.splice(0, n);
+  return arr;
+}
+
+
+Return the remaining elements of an array after chopping off n elements from the head.
+
+The head means the beginning of the array, or the zeroth index.
